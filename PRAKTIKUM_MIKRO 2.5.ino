0daf049534add22990d8efaf0569eeecf9a53571
Aprijal <iskandaraprijal@gmail.com>
2016-08-30 11:19:57 +0700

    Add files via upload

---
+++ PRAKTIKUM_MIKRO 2.5.ino
@@ -0,0 +1,18 @@
+void setup() {
+  // put your setup code here, to run once:
+DDRC = 0x00;
+PORTC = 0x00;
+DDRB = 0x10;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if(bit_is_clear(PINC,0))
+{
+  PORTB = 0x10; 
+} 
+else
+{
+  PORTB = 0x01; 
+  }
+}
